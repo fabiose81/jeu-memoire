aa5ae86baf18f1b0e4257d506b524709834e7736
Fabio Estrela <fabiose@gmail.com>
2017-09-13 10:48:18 -0400

    version :: 1

--- jeu_memoire/ViewController.swift
+++ jeu_memoire/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var uiButton1: UIButton!
+    @IBOutlet weak var uiButton2: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
